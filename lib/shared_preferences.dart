676cb226db030da1c4ff894cfac8b3dd12d2623d
MinhTruongHMT <ninjarua2118@gmail.com>
2023-06-24 22:45:24 +0700

    shared preferences

---
+++ lib/shared_preferences.dart
@@ -0,0 +1,20 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Shared {
+  static SharedPreferences? _sharedpreferences;
+  
+  static Future init() async {
+    _sharedpreferences = await SharedPreferences.getInstance();
+  }
+
+  static void setKey(String key, String value) {
+    _sharedpreferences?.setString(key, value);
+  }
+
+  static String? getString(String key) {
+    return _sharedpreferences?.getString(key);
+  }
+
+}
+
+
